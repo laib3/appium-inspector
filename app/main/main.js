62fef41bccb630ece3b40eb25b25f84b3b8abff2
Lorenzo Laudadio <lorenzo.laudadio@polito.it>
2024-07-26 17:09:17 +0200

    first commit

---
+++ app/main/main.js
@@ -0,0 +1,30 @@
+import {app} from 'electron';
+
+import {installExtensions} from './debug';
+import {getAppiumSessionFilePath} from './helpers';
+import {setupMainWindow} from './windows';
+
+const isDev = process.env.NODE_ENV === 'development';
+
+export let openFilePath = getAppiumSessionFilePath(process.argv, app.isPackaged, isDev);
+
+app.on('open-file', (event, filePath) => {
+  openFilePath = filePath;
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('ready', async () => {
+  if (isDev) {
+    require('electron-debug')();
+    await installExtensions();
+  }
+
+  setupMainWindow({
+    mainUrl: `file://${__dirname}/index.html`,
+    splashUrl: `file://${__dirname}/splash.html`,
+    isDev,
+  });
+});
