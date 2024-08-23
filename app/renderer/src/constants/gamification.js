851f349cea3aced4258fb85d15bf74c4b0ce2337
Lorenzo Laudadio <lorenzo.laudadio@polito.it>
2024-08-23 16:26:22 +0200

    add gamification component with own keys and properties

---
+++ app/renderer/src/constants/gamification.js
@@ -0,0 +1,17 @@
+// Columns in the Gamification information table
+export const GAMIFICATION_INFO_PROPS = {
+  session_id: 'Session ID',
+  session_url: 'Session URL',
+  server_details: 'Server Details',
+  session_length: 'Session Length',
+  session_details: 'Session Details',
+  active_appId: 'Currently Active App ID',
+};
+
+export const GAMIFICATION_INFO_TABLE_PARAMS = {
+  OUTER_KEY: 'gamificationInfo',
+  SESSION_KEY: 'gamificationDetails',
+  SERVER_KEY: 'serverDetails',
+  SCROLL_DISTANCE_Y: 104,
+  COLUMN_WIDTH: 200,
+};
