6fb8d56a3c3875be967579e1f4fc77d8737c207b
Lorenzo Laudadio <lorenzo.laudadio@polito.it>
2024-08-24 20:17:14 +0200

    add the number of interacted widgets to GamificationInfo, remove session code box

--- app/renderer/src/constants/gamification.js
+++ app/renderer/src/constants/gamification.js
@@ -1,11 +1,10 @@
 // Columns in the Gamification information table
 export const GAMIFICATION_INFO_PROPS = {
   session_id: 'Session ID',
-  session_url: 'Session URL',
-  server_details: 'Server Details',
   session_length: 'Session Length',
   session_details: 'Session Details',
   active_appId: 'Currently Active App ID',
+  interacted_widgets: 'Number of interacted widgets'
 };
 
 export const GAMIFICATION_INFO_TABLE_PARAMS = {
